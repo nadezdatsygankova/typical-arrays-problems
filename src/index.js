aec3d0f0b600fd8863e640c328d8c5b24b16f082
Nadezda <nadia_pritchina@gmail.com>
2021-03-15 19:55:25 -0400

    add: new way

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (array === undefined) {
+        return 0;
+    }
+    else if (array.length != 0) {
+        return Math.min.apply(Math, array);
+    }
+    else {
+        return 0;
+    }
+
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array === undefined) {
+        return 0;
+    }
+    else if (array.length === 0) {
+        return 0;
+    }
+    else {
+        return Math.max.apply(Math, array);
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array === undefined) {
+        return 0;
+    }
+    else if (array.length === 0) {
+        return 0;
+    }
+    else {
+        let sum = 0;
+        for (let i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        let average = sum / array.length;
+        return average;
+    }
 }
